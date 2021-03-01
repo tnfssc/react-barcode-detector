067814957f848ce30666faa8b735ac40359e8e70
tnfssc <tnfssc@gmail.com>
2021-03-01 12:08:29 +0530

    Updates

---
+++ precache-manifest.edaa2fe1d4829b333eeb3999ce821da1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6c303b8cf5383e3afc40452cc7bc7fe",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1b6f06d9a78e7fad1d97",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "e2a20358300611faaae9",
+    "url": "./static/js/2.af2b58ed.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.af2b58ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b6f06d9a78e7fad1d97",
+    "url": "./static/js/main.3d9b33e5.chunk.js"
+  },
+  {
+    "revision": "911ada9ef200c0ba1bbd",
+    "url": "./static/js/runtime-main.019ed4fe.js"
+  }
+]);
